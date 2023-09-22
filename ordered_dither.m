f0143c4643d19ea2a4dc02e519166bc5de181441
kevin.nava@externos-cl.cencosud.cl <kevin.nava@externos-cl.cencosud.com.com>
2023-09-22 18:48:05 +0200

    first exc1 threshold done

---
+++ ordered_dither.m
@@ -0,0 +1,4 @@
+function B = ordered_dither(I, D)
+    
+    
+end
---
+++ random_dither.m
@@ -0,0 +1,7 @@
+function B = random_dither(A)
+    size_image = size(A) %to get the size of the image
+    matrix = rand(size_image(1),size_image(2)) %to generate the image with col,row
+    img_updated  = A + matrix  %adding values 
+    threshold = 0.5
+    B = img_updated > threshold
+end
